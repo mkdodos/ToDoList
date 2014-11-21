b04dbef9c41daee805339e81d2eefa211049ad33
mkdodos <mkdodos@gmail.com>
2014-11-21 08:09:30 +0800

    SQLite資料庫練習

---
+++ src/com/example/todolist/DBAccess.java
@@ -0,0 +1,52 @@
+package com.example.todolist;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBAccess extends SQLiteOpenHelper {
+
+	public DBAccess(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String sql = "CREATE TABLE `todolist` ("
+				+ "_id	INTEGER PRIMARY KEY AUTOINCREMENT," + "`date`	TEXT,"
+				+ "`time`	TEXT," + "`title`	TEXT)";
+		db.execSQL(sql);
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("drop table if exists todolist");
+		onCreate(db);
+
+	}
+
+	long add(String date, String time, String title) {
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		values.put("date", date);
+		values.put("time", time);
+		values.put("title", title);
+		long result = db.insert("todolist", null, values);
+		db.close();
+		return result;
+	}
+
+	Cursor getData(String whereClause, String orderBy) {
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor c = db.query("todolist", new String[] { "_id", "date", "time",
+				"title" }, whereClause, null, null, null, orderBy);
+		return c;
+	}
+
+}
---
+++ src/com/example/todolist/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.todolist;
+
+import android.support.v7.app.ActionBarActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class MainActivity extends ActionBarActivity {
+
+	DBAccess access;
+	ListView lv;
+	SimpleCursorAdapter adapter = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		lv = (ListView) findViewById(R.id.lvTodo);
+		access = new DBAccess(this, "schedule", null, 2);
+		access.add("2014-11-22", "0317", "第三");
+		access.add("2014-11-21", "0517", "蔡依林");
+	}
+
+	@Override
+	protected void onResume() {
+		Cursor c=access.getData("date>=strftime('%Y-%m-%d','now')", "date,time");
+		if(adapter==null){
+			adapter=new SimpleCursorAdapter(this,R.layout.list_item,c
+					,new String[]{"date","time","title"},new int[]{R.id.tvDate,R.id.tvTime,R.id.tvTitle});
+			lv.setAdapter(adapter);
+		}else{
+			adapter.changeCursor(c);
+		}
+		super.onResume();	
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
